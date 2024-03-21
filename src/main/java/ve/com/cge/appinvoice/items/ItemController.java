fde7f175cabec5d1fd9152f02ef4e32bb6b331b7
Christopher Gedler <cgedler@gmail.com>
2024-03-20 21:16:29 -0400

    Category CRUD Functional with Angular 20-03-2024 commit

--- src/main/java/ve/com/cge/appinvoice/items/ItemController.java
+++ src/main/java/ve/com/cge/appinvoice/items/ItemController.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import ve.com.cge.appinvoice.config.user.UserResponse;
  */
 @RestController
 @RequestMapping("/item")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class ItemController {
   
     private final CategoryService categoryService;
